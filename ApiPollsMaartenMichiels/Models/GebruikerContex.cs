3c0f115d94fe7f5ff2092a5d8d5d4a9690c50755
mnmpower <mnmmaarten@hotmail.com>
2019-10-11 13:59:12 +0200

    added GebruikerContex.cs

---
+++ ApiPollsMaartenMichiels/Models/GebruikerContex.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPollsMaartenMichiels.Models
+{
+    public class GebruikerContex : DbContext
+    {
+        public GebruikerContex(DbContextOptions<GebruikerContex> options) : base(options) { }
+
+        public DbSet<Gebruiker> Gebruikers { get; set; }
+        public DbSet<Poll> polls { get; set; }
+        public DbSet<PollGebruiker> PollGebruikers { get; set; }
+        public DbSet<PollOptie> PollOpties { get; set; }
+        public DbSet<Stem> Stemen { get; set; }
+        public DbSet<Vriend> Vrienden { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Gebruiker>().ToTable("Gebruiker");
+            modelBuilder.Entity<Poll>().ToTable("Poll");
+            modelBuilder.Entity<PollGebruiker>().ToTable("PollGebruiker");
+            modelBuilder.Entity<PollOptie>().ToTable("PollOptie");
+            modelBuilder.Entity<Stem>().ToTable("Stem");
+            modelBuilder.Entity<Vriend>().ToTable("Vriend");
+        }
+    }
+}
