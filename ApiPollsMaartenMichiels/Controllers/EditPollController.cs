106398b45a705516ec2a6b279aedb3857b2398f2
mnmpower <mnmmaarten@hotmail.com>
2019-11-07 16:59:39 +0100

    updates

---
+++ ApiPollsMaartenMichiels/Controllers/EditPollController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiPollsMaartenMichiels.Models;
+using ApiPollsMaartenMichiels.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPollsMaartenMichiels.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EditPollController : Controller
+    {
+        private readonly GebruikerContex _context;
+
+        public EditPollController(GebruikerContex context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EditPoll/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EditPollViewModel>> GetEditPollViewModel(long id, string zoekterm)
+        {
+            EditPollViewModel editPollVM = new EditPollViewModel();
+
+            Poll poll = await _context.Polls.Where(p => p.PollID == id)
+                .Include(p => p.PollGebruikers)
+                .Include(p => p.PollOpties)
+                .FirstOrDefaultAsync();
+
+            editPollVM.EditPoll = poll;
+            return editPollVM;
+        }
+    }
+}
---
+++ ApiPollsMaartenMichiels/ViewModels/EditPollViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ApiPollsMaartenMichiels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPollsMaartenMichiels.ViewModels
+{
+    public class EditPollViewModel
+    {
+        public Poll EditPoll { get; set; }
+        public ICollection<PollGebruiker> PollGebruikers { get; set; }
+        public ICollection<PollGebruiker> Beheerders { get; set; }
+        public ICollection<PollOptie> PollOpties { get; set; }
+    }
+}
